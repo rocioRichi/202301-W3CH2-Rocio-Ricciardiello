4d9aa439a54443ee7254c74e09fcb10ed7ac5608
rocioRichi <paragit@gmail.com>
2023-02-23 08:16:05 +0100

    Add reducer test 70%, + hook

--- src/core/components/store/hooks.ts
+++
@@ -1,8 +0,0 @@
-import type { RootState, AppDispatch } from "./store";
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-function useDispatch<T>() {
-  throw new Error("Function not implemented.");
-}
