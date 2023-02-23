0b71e5b5ab0a508956441cf58ae4654d45eb4560
rocioRichi <paragit@gmail.com>
2023-02-23 07:44:37 +0100

    Add reducer

--- src/feature/reducer/actioncreator.ts
+++ src/feature/reducer/actioncreator.ts
@@ -2,6 +2,6 @@ import { createAction } from "@reduxjs/toolkit";
 import { actions } from "./actions";
 export const callCreator = createAction(actions.call);
 
-export const inputCreator = createAction<string>(actions.dialedNumber);
+export const dialedCreator = createAction<string>(actions.dialedNumber);
 
 export const deleteCreator = createAction(actions.deleteNumber);
--- src/feature/reducer/reducer.ts
+++ src/feature/reducer/reducer.ts
@@ -1,14 +1,17 @@
+import { createReducer } from "@reduxjs/toolkit";
+import * as ac from "./actioncreator";
+
 type state = {
   number: string;
   isCalling: boolean;
 };
 
-const initialState: state = { number: "", isCalling: false };
+const state: state = { number: "", isCalling: false };
 
-export const phoneReducer = createReducer(initialState, (builder) => {
-  builder.addCase(ac.inputCreator, (state, { payload }) => {
-    const newNumber = state.number + payload;
-    return { ...state, number: newNumber };
+export const Reducer = createReducer(state, (builder) => {
+  builder.addCase(ac.dialedCreator, (state, { payload }) => {
+    const addedNum = state.number + payload;
+    return { ...state, number: addedNum };
   });
   builder.addCase(ac.callCreator, (state, { payload }) => {
     const call = !state.isCalling;
